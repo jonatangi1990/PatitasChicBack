77345b7e02a94c2f1b2db95b2ac0b81cc891e2c2
Jonatan <jgutierreziglesias@gmail.com>
2025-09-30 11:52:47 +0200

    conexion base de datos

---
+++ BBDD/config.py
@@ -0,0 +1,15 @@
+import os
+import aiomysql
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+async def get_conexion():
+    return await aiomysql.conect(
+        host=os.getenv("MYSQL_HOST", "localhost"),
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        user=os.getenv("MYSQL_USER", "root"),
+        password=os.getenv("MYSQL_PASSWORD", "Campon8011"),
+        db=os.getenv("MYSQL_DATABASE", "patitaschic")
+    )
