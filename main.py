469e9b1829eac56994cc376b6427e9bde7115ae8
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-09-30 13:10:14 +0200

    peticion Get de Servicios de BBDD
    Testing para que muestre la lista de servicios

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routes import service_routes
 
 app = FastAPI(debug=True)
 
 #creo las rutas
-
+# Creo acceso a la ruta service
+app.include_router(service_routes.router,
+                   prefix="/service",
+                   tags=['Service'])
 
 # Hago la conexión entre el back y el front
 app.add_middleware(
---
+++ models/service_model.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+class Service(BaseModel):
+    id: int
+    name: str
+    description: str
+    duration: int
