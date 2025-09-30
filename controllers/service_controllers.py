448b84c75d08c20278de0dadfcb81e7d5c999058
Maria Patricia Ruiz de Pedraza <patricia.ruiz6248@gmail.com>
2025-09-30 13:19:14 +0200

    Peticion GET del service
    WORKS!!!!!

--- controllers/service_controllers.py
+++ controllers/service_controllers.py
@@ -7,13 +7,14 @@ import aiomysql
 
 #_______________________________GET___________________________________
 
-async def get_categorie_list():
+async def get_service_list():
     try:
         conn=await get_conexion()
         async with conn.cursor(aiomysql.DictCursor) as cursor:
             await cursor.execute('SELECT * FROM patitaschic.service')
             data = await cursor.fetchall()
-
+        return data
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
     
